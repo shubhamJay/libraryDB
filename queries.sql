44280b688d767230cf09fe0dcb7928f3b0f56866
Shubham Jaybhaye <shubhamj7997@gmail.com>
2018-03-19 12:39:14 +0530

    added starting qeuries and csv files

---
+++ queries.sql
@@ -0,0 +1,30 @@
+
+create view all_book_details as select b.book_id ,d.* from books b join book_details d
+  on b.book_name = d.book_name;
+
+
+create view count_of_book as select b.book_name, count(b.book_name)
+  as number_of_book from books b join book_details d on b.book_name = d.book_name
+  group by b.book_name;
+
+
+2) select * from count_of_book order by number_of_book desc;
+
+3) select * from count_of_book where number_of_book > 5;
+
+create view transaction_with_book_and_user_id as select t.* , a.book_name
+  from all_book_details a join transaction t on a.book_id = t.book_id;
+
+
+4) select book_name, count(book_name) as count1 from transaction_with_book_and_user_id
+  where borrowed_on>'2017-06-01' and borrowed_on<'2017-07-01' group by book_name
+  order by count(book_name);
+
+
+7) select user_id, count(user_id) from transaction_with_book_and_user_id
+  where borrowed_on>'2017-06-01' and borrowed_on<'2017-07-01' group by user_id
+  order by count(user_id) desc limit 1;
+
+
+  
+ -- select * from (select d.user_id,b.book_id,b.book_name from all_book_details b join transaction d on b.book_id = d.book_id where d.borrowed_on>'2017-06-01' and d.borrowed_on<'2017-07-01') group by book_name
---
+++ start.sql
@@ -0,0 +1,27 @@
+create schema library;
+
+set search_path to library;
+
+create table books (book_id numeric(5) primary key, ISBN varchar(13),availability boolean default true);
+
+create table Book_details (ISBN varchar(13) primary key, book_name varchar(40),
+  Author varchar(40) not null default 'unknown', Publisher varchar(400) not null default 'unknown',
+  description Text ,type varchar(20) not null
+  );
+
+  create table users (user_id varchar(20) primary key,password varchar(20) not null, name varchar(40) not null,
+  contact_no numeric(10) not null, email_id varchar(40) not null);
+
+
+  create table transaction (transaction_id numeric(5) primary key, user_id varchar(20) references users(user_id) ,
+  book_id numeric(5) references books(book_id), borrowed_on date, returned_on date);
+
+
+
+alter table books add constraint fk_to_books foreign key (ISBN) references Book_details(ISBN);
+
+COPY Book_details from '/Users/shubhaj/Downloads/Book_details.csv' with delimiter ',';
+COPY books from '/Users/shubhaj/Downloads/Books.csv' with delimiter ',';
+
+
+DROP schema library cascade;
