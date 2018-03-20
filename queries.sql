56fd5a4e5b8bc132ac368416bf1f778f04501f5c
sridevs <senthilnathansridev@gmail.com>
2018-03-20 10:59:45 +0530

    added query to show all titles available in library

--- queries.sql
+++ queries.sql
@@ -1,28 +1,12 @@
-
-create view all_book_details as select b.book_id,b.availability,d.* from books b join book_details d
-  on b.isbn = d.isbn;
-
-1 - select * from  all_book_details where availability = false;
-
-create view count_of_book as select b.isbn, count(b.isbn)
-  as number_of_book from books b join book_details d on b.isbn = d.isbn
-  group by b.isbn;
-
+1) select distinct book_name from book_details;
 
 2) select * from count_of_book order by number_of_book desc;
 
 3)select * from count_of_book where number_of_book<5 order by number_of_book desc;
 
-create view transaction_with_book_and_user_id as select t.* , a.book_name
-  from all_book_details a join transaction t on a.book_id = t.book_id;
-
-
 4) select book_name, count(book_name) as count1 from transaction_with_book_and_user_id
   where borrowed_on>'2017-06-01' and borrowed_on<'2017-07-01' group by book_name
   order by count(book_name) desc;
-
-create view transaction_with_all_book as select t.* , a.book_name
-  from all_book_details a left join transaction t on a.book_id = t.book_id;
 
 -- 5) select not book_name from transaction_with_all_book
 --   where not borrowed_on>'2017-06-01';
