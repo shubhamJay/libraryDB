6e26e697185f8abde5a444541845f817d8f7b168
Praveen Kumar Gupta <guptapraveen@gmail.com>
2018-03-21 11:20:39 +0530

    changes in date of in  all_transaction_of_before_june of date

--- start.sql
+++ start.sql
@@ -34,7 +34,7 @@ create view transaction_with_all_book as select t.* , a.book_name
   from all_book_details a left join transaction t on a.book_id = t.book_id;
 
 create view all_transaction_of_before_june as select user_id,count(user_id)
-  from transaction_with_book_and_user_id   where borrowed_on<'2017-06-20'
+  from transaction_with_book_and_user_id   where borrowed_on<'2017-06-30'
   and returned_on is null group by user_id;
 ------------------------------END----------------------------------------------
 \set path '\'':p'/Book_details.csv\''
